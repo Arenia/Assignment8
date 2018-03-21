1873355e39a56d28adde580d515dea3ca855f995
Arenia <tyler.bagala@gmail.com>
2018-03-21 13:19:06 -0600

    Finished testing and adjustments

--- calculator.js
+++ calculator.js
@@ -92,10 +92,12 @@ function clickSelectedItem() {
 // this function responds to user key presses
 // you'll rewrite this to control your interface using some number of keys
 $(document).keypress(function(event) {
-	if (event.key == "a") {
-		alert("You pressed the 'a' key!")	
-	} else if (event.key == "b") {
-		alert("You pressed the 'b' key!")
+	if (event.key == "a" || event.key == "A") {
+		selectPrevious();
+	} else if (event.key == "d" || event.key == "D") {
+		selectNext();
+	} else if(event.key == "s" || event.key == "S") {
+		clickSelectedItem();
 	}
 })
 
